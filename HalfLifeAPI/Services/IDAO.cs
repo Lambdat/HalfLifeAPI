99a9612082d399daa79fd95fbaf0bbbbd1b9ea88
Lambdat <oreficeantonio98@postecert.it>
2021-10-07 11:25:15 +0200

    Interfaccia IDAO Aggiunta

---
+++ HalfLifeAPI/Services/IDAO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Utility;
+
+namespace HalfLifeAPI.Services
+{
+    interface IDAO
+    {
+
+        public List<Entity> Leggi();
+
+        public Entity Cerca();
+
+        public void Aggiungi();
+
+        public void Modifica();
+
+        public void Elimina(int id);
+
+    }
+}
