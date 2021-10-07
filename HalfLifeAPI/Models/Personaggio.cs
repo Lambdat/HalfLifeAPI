16f687f9cc05c242574ecd37941b5442e7b5050e
Lambdat <oreficeantonio98@postecert.it>
2021-10-07 10:32:44 +0200

    Creazione Modello Classe Personaggio

---
+++ HalfLifeAPI/Models/Personaggio.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfLifeAPI.Models
+{
+    public class Personaggio
+    {
+        public string Nome { get; set; }
+        public int Potenza { get; set; }
+        public DateTime Dob { get; set; }
+
+
+
+
+
+
+    }
+}
